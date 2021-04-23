cd46355bc9ea93107af3ea3588d640836d857b79
Egor Shulga <egor.shulga@nezaboodka.com>
2021-04-23 11:44:35 +0300

    add datepicker as regular component, and as inputView for textField

---
+++ Pods/Target Support Files/Pods-datepicker-debugging/Pods-datepicker-debugging-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_datepicker_debugging : NSObject
+@end
+@implementation PodsDummy_Pods_datepicker_debugging
+@end
--- datepicker-debugging/ViewController.swift
+++ datepicker-debugging/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var field: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        field.delegate = self
     }
-
-
 }
 
+extension ViewController : UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField == field {
+            let datePicker = UIDatePicker()
+            datePicker.datePickerMode = .dateAndTime
+            datePicker.preferredDatePickerStyle = .wheels
+            field.inputView = datePicker
+        }
+    }
+}
